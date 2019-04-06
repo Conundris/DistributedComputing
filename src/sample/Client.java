effcc304f210b0cf8099b443d77fae27095c1898
Conundris <jason.h.brockett@gmail.com>
2019-04-06 17:41:26 +0200

    Cleanup

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -4,6 +4,7 @@ import sample.SSL.SSLClient;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
+import java.awt.image.ReplicateScaleFilter;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
@@ -74,7 +75,7 @@ public class Client {
             System.out.println(user.getUsername() + " logging out.");
             serverResult = logout(user.getUsername(), user.getPassword());
 
-            if(Utils.extractResponseCode(serverResult) == ResponseCode.USER_LOGGED_OUT_SERVICE_TERMINATED) {
+            if(Utils.extractResponseCode(serverResult) == ResponseCode.USER_LOGGED_OUT) {
                user.setUsername("");
                user.setPassword("");
                user.setLoggedIn(false);
@@ -118,39 +119,42 @@ public class Client {
             int fileToDownload = -1;
             List<String> files = getFileListForUser();
 
-            //File Selection Loop
-            while(!selectedFile) {
+            if(files != null) {
+               //File Selection Loop
+               while(!selectedFile) {
 
-               for (int i = 0; i < files.size(); i++) {
-                  System.out.println(i + ": " + files.get(i));
+                  for (int i = 0; i < files.size(); i++) {
+                     System.out.println(i + ": " + files.get(i));
+                  }
+
+                  System.out.println("Enter which file you wish to download");
+                  String answer = br.readLine();
+
+                  try {
+                     fileToDownload = Integer.parseInt(answer.trim());
+                     selectedFile = true;
+                  } catch (NumberFormatException nfe) {
+                     System.out.println("NumberFormatException: " + nfe.getMessage());
+                     System.out.println("Please input only a number.");
+                     fileToDownload = -1;
+                  }
                }
 
-               System.out.println("Enter which file you wish to download");
-               String answer = br.readLine();
+               if(fileToDownload != -1){
 
-               try {
-                  fileToDownload = Integer.parseInt(answer.trim());
-                  selectedFile = true;
-               } catch (NumberFormatException nfe) {
-                  System.out.println("NumberFormatException: " + nfe.getMessage());
-                  System.out.println("Please input only a number.");
-                  fileToDownload = -1;
+                  JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory() + "\\" + files.get(fileToDownload));
+                  int returnValue = jfc.showSaveDialog(null);
+
+                  if(returnValue == JFileChooser.APPROVE_OPTION) {
+                     selectedFileToSave = jfc.getSelectedFile();
+                     System.out.println(selectedFileToSave.getName());
+                  }
+
+                  download(user.getUsername(), files.get(fileToDownload), selectedFileToSave);
                }
+            } else {
+               System.out.println("Retrieving list of files for user: " + user.getUsername() + " was unsuccessful.");
             }
-
-            if(fileToDownload != -1){
-
-               JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory() + "\\" + files.get(fileToDownload));
-               int returnValue = jfc.showSaveDialog(null);
-
-               if(returnValue == JFileChooser.APPROVE_OPTION) {
-                  selectedFileToSave = jfc.getSelectedFile();
-                  System.out.println(selectedFileToSave.getName());
-               }
-
-               download(user.getUsername(), files.get(fileToDownload), selectedFileToSave);
-            }
-
             break;
          case "4": //Quit
             System.out.println("Quitting Application.");
@@ -172,11 +176,18 @@ public class Client {
       String result = client.sendAndReceive(ByteBuffer.wrap(message.getBytes()), "localhost", 3000).getMessage().trim();
 
       System.out.println("Response received" + result);
-      FileOutputStream fos = new FileOutputStream(path);
-      fos.write(Base64.getDecoder().decode(result));
-      fos.close();
 
-      System.out.println("File Downloaded to this destination: " + path);
+      String[] splitresult = result.split(":");
+
+      if(Short.parseShort(splitresult[0].trim()) == ResponseCode.DOWNLOAD_SUCCESSFUL) {
+         FileOutputStream fos = new FileOutputStream(path);
+         fos.write(Base64.getDecoder().decode(splitresult[1].trim()));
+         fos.close();
+
+         System.out.println("File Downloaded to this destination: " + path);
+      } else {
+         System.out.println("File Has not been downloaded successfully, ERROR");
+      }
    }
 
    public static String upload(String username, File file) throws IOException {
@@ -200,10 +211,18 @@ public class Client {
    private static List<String> getFileListForUser() throws IOException, ClassNotFoundException {
       String returnVal = client.sendAndReceive(ByteBuffer.wrap((RequestCode.DDLIST +"§ " + user.getUsername()).getBytes()), "localhost", 3000).getMessage().trim();
 
-      ByteArrayInputStream in = new ByteArrayInputStream(Base64.getDecoder().decode(returnVal));
-      ObjectInputStream is = new ObjectInputStream(in);
+      String[] splitReturnVal = returnVal.split(":");
 
-      return (List<String>) is.readObject();
+      if(Short.parseShort(splitReturnVal[0].trim()) == ResponseCode.USER_FILES_LISTED) {
+         System.out.println(splitReturnVal[0] + ": FileList retrieved.");
+
+         ByteArrayInputStream in = new ByteArrayInputStream(Base64.getDecoder().decode(splitReturnVal[1].trim()));
+         ObjectInputStream is = new ObjectInputStream(in);
+
+         return (List<String>) is.readObject();
+      } else {
+         System.out.println(splitReturnVal[0] + ": FileList couldn't be retrieved.");
+         return null;
+      }
    }
-
 }
